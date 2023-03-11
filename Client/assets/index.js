7c3b288fc59cdbd1560e468a8a4b36ef7239b14a
David Herring <davidherring@Davids-MBP.home>
2023-03-11 01:12:11 -0600

    adding review data to welcome page from database

---
+++ Client/assets/index.js
@@ -0,0 +1,32 @@
+const nameOne = document.getElementById('name-one');
+const nameTwo = document.getElementById('name-two');
+const nameThree = document.getElementById('name-three');
+const bodyOne = document.getElementById('body-one')
+const bodyTwo = document.getElementById('body-two')
+const bodyThree = document.getElementById('body-three')
+
+// API call for review data 
+const reviewAPI = fetch("http://localhost:8000/api/reviews")
+  .then((res) => res.json())
+  .then((data) => {
+    return data;
+  })
+
+const getReviews = async () => {
+  const data = await reviewAPI;
+
+  console.log(data[1].body)
+  // Text box one
+  bodyOne.innerHTML = data[0].body
+  nameOne.innerHTML = `-${data[0].name}`
+
+  // Text box one
+  bodyTwo.innerHTML = data[1].body
+  nameTwo.innerHTML = `-${data[1].name}`
+
+  // Text box one
+  bodyThree.innerHTML = data[2].body
+  nameThree.innerHTML = `-${data[2].name}`
+}
+
+getReviews();
